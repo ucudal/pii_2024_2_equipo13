9bd78bf8b7af5702f839de070957c297a06b1f20
Josue2kMR <josuernesto05@gmail.com>
2024-11-04 13:19:59 -0300

    Se agregaron funciones básicas del waitlist

--- proyectoChatbot/src/Library/Clases/WaitList.cs
+++ proyectoChatbot/src/Library/Clases/WaitList.cs
@@ -1,6 +1,43 @@
+using System.Net.Sockets;
+
 namespace Library.Clases;
 
 public class WaitList
 {
+    private List<Jugador> waitListJugador;
+    
+    public List<Jugador> WaitListJugador
+    {
+        get { return waitListJugador; }
+    }
+
+    public WaitList()
+    {
+        waitListJugador = new List<Jugador>();
+    }
+    public void SalaDeEspera(Jugador jugador)
+    { 
+        waitListJugador.Add(jugador);
+        Console.WriteLine($"{jugador.Nombre} ha sido agregado a la lista de espera.");
+    }
+
+    public void StartBattle()
+    {
+        if (waitListJugador.Count < 2)
+        {
+            Console.WriteLine("No hay suficientes jugadores para iniciar una batalla.");
+            return;
+        }
+        Console.WriteLine("Iniciando...");
+    }
+    
+    public void ImprimirLista()
+    {
+        Console.WriteLine($"La lista contiene un total de {waitListJugador.Count} elementos:\n");
+        for (int i = 0; i < waitListJugador.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {waitListJugador[i]}");
+        }
+    }
     
 }
