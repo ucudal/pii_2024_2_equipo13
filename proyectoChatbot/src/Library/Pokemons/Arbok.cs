49427626669db7ab6f714cd60481bec7aa905e60
ConstanLopez <clopezreich@gmail.com>
2024-11-06 10:01:40 -0300

    arcanine

--- proyectoChatbot/src/Library/Pokemons/Arbok.cs
+++ proyectoChatbot/src/Library/Pokemons/Arbok.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using Library.EfectosAtaque;
 using Library.TiposPokemon;
 
 namespace Library.Pokemons;
@@ -14,7 +15,16 @@ public class Arbok : Pokemon
         this.VidaMax = 100;
         this.Tipo =  new Veneno();
         this.AptoParaBatalla = true;
-        this.AtaquesBasicos = new Dictionary<int, IAtaque>();
-        this.AtaquesEspeciales = new Dictionary<int, IAtaque>();
+        this.AtaquesBasicos = new Dictionary<int, IAtaque>
+        {
+            {1,new AtaqueBasico("Mordisco", 55, new Veneno(),90)},
+            {2,new AtaqueBasico("Ácido", 50, new Veneno(),90)}
+            
+        };
+        this.AtaquesEspeciales = new Dictionary<int, IAtaque>
+        {
+            {1,new AtaqueEspecial("Bomba Lodo", 90, new Veneno(),100,new Envenenar(0.3))},
+            {2,new AtaqueEspecial("Onda Tóxica", 95, new Veneno(),100,new Envenenar(0.1))}
+        };
     }
 }
--- proyectoChatbot/src/Library/Pokemons/Arcanine.cs
+++ proyectoChatbot/src/Library/Pokemons/Arcanine.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using Library.EfectosAtaque;
 using Library.TiposPokemon;
 
 namespace Library.Pokemons;
@@ -15,8 +16,17 @@ public class Arcanine : Pokemon
         this.VidaMax = 100;
         this.Tipo = new Fuego();
         this.AptoParaBatalla = true;
-        this.AtaquesBasicos = new Dictionary<int, IAtaque>();
-        this.AtaquesEspeciales = new Dictionary<int, IAtaque>();
+        this.AtaquesBasicos = new Dictionary<int, IAtaque>
+        {
+            { 1, new AtaqueBasico("Golpe Roca", 50, new Lucha(), 90) },
+            { 2, new AtaqueBasico("Derribo",70, new Normal(), 90) }
+        };
+        this.AtaquesEspeciales = new Dictionary<int, IAtaque>
+        {
+            
+            {1,new AtaqueEspecial("Rueda Fuego", 80, new Veneno(),100,new Quemar(0.1))},
+            {2,new AtaqueEspecial("Colmillo Igneo", 85, new Veneno(),100,new Quemar(0.1))}
+        };
     }
 
     
